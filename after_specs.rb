ac19dec1935b4e60384a73a60e7b02fcce32b038
Kevin Liddle <kevin@8thlight.com>
2014-10-06 20:49:09 -0500

    Make after_specs.rb work for rspec 2 or 3

--- after_specs.rb
+++ after_specs.rb
@@ -1,13 +1,44 @@
-RSpec.configure do |config|
-  failures = false
+class MyRSpec
 
-  config.after(:each) do |example|
-    if example.exception
-      failures = true
+  def self.v3_rspec(config)
+    failures = false
+
+    config.after(:each) do |example|
+      if example.exception
+        failures = true
+      end
+    end
+
+    config.after(:suite) do
+      Thread.new { failures ? `trolleap` : `unicornleap` }
     end
   end
 
-  config.after(:suite) do
+  def self.v2_rspec(config)
+    failures = false
+
+    config.after(:each) do
+      if example.exception
+        failures = true
+      end
+    end
+
+    config.after(:suite) do
+      MyRSpec.leap(failures)
+    end
+  end
+
+  def self.leap(failures)
     Thread.new { failures ? `trolleap` : `unicornleap` }
+  end
+
+end
+
+RSpec.configure do |config|
+  case RSpec::Core::Version::STRING.split(".").first.to_i
+  when 2
+    MyRSpec.v2_rspec(config)
+  when 3
+    MyRSpec.v3_rspec(config)
   end
 end
